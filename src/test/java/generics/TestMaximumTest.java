7b9de26c82006a1c95bdd67c6e3625f083c07c80
surjya <surjyamaniduan@gmail.com>
2021-10-10 20:28:40 +0530

    Add:find float

--- src/test/java/generics/TestMaximumTest.java
+++ src/test/java/generics/TestMaximumTest.java
@@ -1,8 +1,6 @@
 package generics;
-
 import org.junit.Assert;
 import org.junit.Test;
-
 public class TestMaximumTest {
       @Test
       public void givenFirstNumberAsGreater_ShouldReturnTrue() {
@@ -15,8 +13,26 @@ public class TestMaximumTest {
             Assert.assertEquals(100,testMaximum.findMaximum());
       }
       @Test
-      public void givenThirdNumberAsGreater_ShoulReturnTrue() {
+      public void givenThirdNumberAsGreater_ShouldReturnTrue() {
             TestMaximum testMaximum = new TestMaximum(20,30,100);
             Assert.assertEquals(100,testMaximum.findMaximum());
+      }
+
+      @Test
+      public void givenFloatFirstNumberAsGreater_ShouldReturnTrue() {
+            TestMaximum testMaximum = new TestMaximum(100.2f,20.4f,30.5f);
+            Assert.assertEquals(100.2f,testMaximum.findMaximumFloat(),0.0f);
+      }
+
+      @Test
+      public void givenSecondFloatNumberAsGreater_ShouldReturnTrue() {
+            TestMaximum testMaximum = new TestMaximum(20.4f,100.2f,30.5f);
+            Assert.assertEquals(100.2f,testMaximum.findMaximumFloat(),0.0f);
+      }
+
+      @Test
+      public void givenThirdFloatNumberAsGreater_ShouldReturnTrue() {
+            TestMaximum testMaximum = new TestMaximum(20.4f,30.5f,100.2f);
+            Assert.assertEquals(100.2f,testMaximum.findMaximumFloat(),0.0f);
       }
 }
